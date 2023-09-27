77ed376d7fe4ffa3b7c9ab0e1f956150eae9a6d0
Ricardo Alencar <ricfilho0007@gmail.com>
2023-09-27 15:13:36 -0300

    fim receita fetch

--- fetch/script.js
+++ fetch/script.js
@@ -1,37 +1,38 @@
-let cervejas = []
-let users = []
-
-
-    const carregarDiv = cs => {
-    const div = document.getElementById("usersDiv")
-    const itensHtml = cs.map( ({first_name, username, email}) => `
-        <tr>
-            <td>${first_name}</td>
-            <td>${username}</td>
-            <td>${email}</td>
-        </tr>
-    ` ) 
-    div.innerHTML = `
-        <table>
-            <tr>
-                <th>Nome</th>
-                <th>Username</th>
-                <th>E-mail</th>
-            </tr>
-            ${itensHtml.join("\n")}
-        </table>
-    `
+async function generateTable(endpoint) {
+    try {
+      const response = await fetch(endpoint);
+      const data = await response.json();
+  
+      if (!data || data.length === 0) {
+        throw new Error('Nenhum dado disponível.');
+      }
+  
+      const keys = Object.keys(data[0]);
+      const table = document.createElement('table');
+      const headerRow = document.createElement('tr');
+  
+      keys.forEach((key) => {
+        const th = document.createElement('th');
+        th.textContent = key;
+        headerRow.appendChild(th);
+      });
+  
+      table.appendChild(headerRow);
+  
+      data.forEach((item) => {
+        const row = document.createElement('tr');
+        keys.forEach((key) => {
+          const cell = document.createElement('td');
+          cell.textContent = item[key];
+          row.appendChild(cell);
+        });
+        table.appendChild(row);
+      });
+  
+      return table;
+    } catch (error) {
+      console.error('Erro ao gerar a tabela:', error);
+      return null;
     }
-
-    async function carregarUsuarios(){
-        try{
-            let res = await fetch("https://random-data-api.com/api/v2/users?size=3")
-            usuarios = await res.json()
-            carregarDiv(usuarios)
-        }catch(err){
-            document.getElementById("usersDiv").innerHTML = "Nenhum dado encontrado..."
-        }
-    }
-
-    let botao = document.getElementById("botaoCarregar")
-    botao.addEventListener("click", () => carregarUsuarios() )
+  }
+  
