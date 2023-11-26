a9a197d26ac527192f3cc95046130351109a62c3
Ricardo Alencar <ricfilho0007@gmail.com>
2023-11-26 11:12:54 -0300

    fim receita 1 next

---
+++ unidade3/next0/pages/receita1/index.js
@@ -0,0 +1,9 @@
+import { Title } from "./title"
+
+export default function Home() {
+    return(
+        <div>
+            <Title />
+        </div>
+    )
+}
---
+++ unidade3/next0/pages/receita1/title.js
@@ -0,0 +1,8 @@
+
+export function Title() {
+    return(
+        <div>
+            <h1>Título do componente</h1>
+        </div>
+    )
+}
