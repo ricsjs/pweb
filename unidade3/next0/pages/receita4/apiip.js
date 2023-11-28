90195ddc08a9bab659c3d38c20eacadd131a0bb9
Ricardo Alencar <ricfilho0007@gmail.com>
2023-11-28 15:22:47 -0300

    fim receita 4 next

--- unidade3/next0/pages/receita4/apiip.js
+++ unidade3/next0/pages/receita4/apiip.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const ApiIp = () => {
-    const [ipInfo, setIpInfo] = useState(null);
+    const [ipDetails, setIpDetails] = useState(null);
 
     useEffect(() => {
         const apiIp = async () => {
             try {
                 const response = await axios.get("https://extreme-ip-lookup.com/json/?key=mKFXhfb8Ip6jFKdEbuYg");
-                setIpInfo(response.data);
+                setIpDetails(response.data);
             } catch (error) {
                 console.error("Nenhum dado encontrado", error);
             }
@@ -17,15 +17,15 @@ const ApiIp = () => {
         apiIp();
     }, []);
 
-    if (!ipInfo) return <div>Carregando...</div>;
+    if (!ipDetails) return <div>Carregando...</div>;
 
     return (
         <div>
             <h2>Informações do IP</h2>
-            <p><strong>IP:</strong> {ipInfo.query}</p>
-            <p><strong>Cidade:</strong> {ipInfo.city}</p>
-            <p><strong>Região:</strong> {ipInfo.region}</p>
-            <p><strong>País:</strong> {ipInfo.country}</p>
+            <p><b>IP:</b> {ipDetails.query}</p>
+            <p><b>Cidade:</b> {ipDetails.city}</p>
+            <p><b>Região:</b> {ipDetails.region}</p>
+            <p><b>País:</b> {ipDetails.country}</p>
         </div>
     );
 };
